509794d8e608927a9ec9501c678af71f541c161b
FaberSanZ <faber.reach@gmail.com>
2025-08-06 23:30:10 -0500

    Minimal example of how to start Vulkan and clear the buffer or colorAttachment of a solid color

--- Src/VulkanApp/VulkanApp.cpp
+++ Src/VulkanApp/VulkanApp.cpp
@@ -12,7 +12,7 @@
 #include <vulkan/vulkan_win32.h>
 
 #include <glfw3.h>
-#include <glfw3native.h> // 👈 Obligatorio para funciones nativas
+#include <glfw3native.h>
 
 
 struct Vector2
@@ -27,158 +27,220 @@ struct Vector2
 	Vector2 operator*(float scalar) const { return Vector2(x * scalar, y * scalar); }
 };
 
-struct QueueFamilyIndices
-{
-	uint32_t index { 0 }; // default family index for graphics queue and compute queue
-};
-
-VkInstance instance;
-VkPhysicalDevice gpu;
-VkDevice device;
-VkSurfaceKHR surface;
-
-VkPhysicalDeviceFeatures deviceFeatures {};
-VkQueue queue;
-
-
-
-void CreateInstance()
-{
-    VkApplicationInfo appInfo = {};
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-
-	// extensions for Win32 platform
-	// these are required for Vulkan to work on Windows
-	// TODO: add extensions for other platforms
-	const char* instanceExtensions[] =
-	{
-		"VK_KHR_surface",
-		"VK_KHR_win32_surface"
-	};
-
-	VkInstanceCreateInfo createInfo = {};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo = &appInfo;
-	createInfo.enabledExtensionCount = sizeof(instanceExtensions) / sizeof(instanceExtensions[0]);
-	createInfo.ppEnabledExtensionNames = instanceExtensions;
-
-	vkCreateInstance(&createInfo, nullptr, &instance);
-
-}
-
-
-void SelectGPU(uint32_t index)
-{
-	// This function would typically create a Vulkan device and select a physical GPU.
-	// For simplicity, we are not implementing this in this example.
-
-	uint32_t physicalDeviceCount = 0;
-	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
-
-	std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
-
-	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data());
-
-
-	gpu = physicalDevices[index]; // Select the first physical device
-}	
-
-void CreateDevice()
-{
-	// This function would typically create a Vulkan logical device.
-	// For simplicity, we are not implementing this in this example.
-	// You would need to query the queue family indices and create a VkDevice.
-	QueueFamilyIndices queueIndices {};
-	float queuePriority = 1.0f;
-
-
-	VkDeviceQueueCreateInfo queueCreateInfo {};
-	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-	queueCreateInfo.queueFamilyIndex = queueIndices.index;
-	queueCreateInfo.queueCount = 1;
-	queueCreateInfo.pQueuePriorities = &queuePriority;
-
-	VkDeviceCreateInfo createInfo {};
-	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-
-	createInfo.pQueueCreateInfos = &queueCreateInfo;
-	createInfo.queueCreateInfoCount = 1;
-	createInfo.pEnabledFeatures = &deviceFeatures;
-
-
-	vkCreateDevice(gpu, &createInfo, nullptr, &device);
-
-
-	vkGetDeviceQueue(device, queueIndices.index, 0, &queue);
-}
-
-void CreateSurface(HWND handle)
-{
-	// This function would typically create a Vulkan surface for rendering.
-	// For simplicity, we are not implementing this in this example.
-	// You would need to use platform-specific APIs to create a surface.
-	// For GLFW, you can use glfwCreateWindowSurface().
-
-	VkWin32SurfaceCreateInfoKHR surfaceCreateInfo = {};
-	surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
-	surfaceCreateInfo.hwnd = (HWND)handle;
-	surfaceCreateInfo.hinstance = GetModuleHandle(nullptr); // o pasarla como global
-
-	VkSurfaceKHR surface;
-	VkResult result = vkCreateWin32SurfaceKHR(instance, &surfaceCreateInfo, nullptr, &surface);
-	if (result != VK_SUCCESS)
-	{
-		// Manejar error
-	}
-}
-
-
-void InitializeVulkan(HWND handle)
-{
-	CreateInstance();
-	SelectGPU(0); // Select the first GPU (index 0)
-	CreateDevice();
-	CreateSurface(handle); // Create a Vulkan surface using GLFW
-
-	std::cout << "Instance: " << instance << std::endl;
-	std::cout << "gpu: " << gpu << std::endl;
-	std::cout << "device: " << device << std::endl;
-	std::cout << "queue: " << queue << std::endl;
-	std::cout << "surface: " << surface << std::endl;
-}
-
-void Destroy()
-{
-	if (instance != VK_NULL_HANDLE) {
-		vkDestroyInstance(instance, nullptr);
-	}
-}
 
 int main()
 {
-	uint32_t width = 800;
-	uint32_t height = 600;
+	uint32_t width = 1200;
+	uint32_t height = 820;
 
-
+	// 0. GLFW Window
 	glfwInit();
-
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 	GLFWwindow* window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+    HWND hwnd = glfwGetWin32Window(window);
 
 
-	InitializeVulkan(glfwGetWin32Window(window));
+    // 1. Instance
+    VkApplicationInfo appInfo { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    const char* extensions[] = { "VK_KHR_surface", "VK_KHR_win32_surface" };
+
+    VkInstanceCreateInfo instanceInfo { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+    instanceInfo.pApplicationInfo = &appInfo;
+    instanceInfo.enabledExtensionCount = 2;
+    instanceInfo.ppEnabledExtensionNames = extensions;
+
+    VkInstance instance;
+    vkCreateInstance(&instanceInfo, nullptr, &instance);
+
+    // 2. Surface (Win32 handle)
+    VkSurfaceKHR surface;
+    VkWin32SurfaceCreateInfoKHR surfaceCreateInfo { VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR };
+    surfaceCreateInfo.hwnd = hwnd;
+    surfaceCreateInfo.hinstance = GetModuleHandle(nullptr);
+    vkCreateWin32SurfaceKHR(instance, &surfaceCreateInfo, nullptr, &surface);
+
+    // 3. Physical + Logical Device
+    uint32_t gpuCount;
+    vkEnumeratePhysicalDevices(instance, &gpuCount, nullptr);
+    std::vector<VkPhysicalDevice> gpus(gpuCount);
+    vkEnumeratePhysicalDevices(instance, &gpuCount, gpus.data());
+
+    VkPhysicalDevice gpu = gpus[0];
+
+    uint32_t queueFamilyIndex = 0;
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueInfo { VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
+    queueInfo.queueFamilyIndex = queueFamilyIndex;
+    queueInfo.queueCount = 1;
+    queueInfo.pQueuePriorities = &queuePriority;
+
+    const char* deviceExtensions[] = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME
+    };
+
+    VkPhysicalDeviceDynamicRenderingFeatures dynamicFeature { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES };
+    dynamicFeature.dynamicRendering = VK_TRUE;
+
+    VkDeviceCreateInfo deviceInfo { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
+    deviceInfo.pNext = &dynamicFeature;
+    deviceInfo.queueCreateInfoCount = 1;
+    deviceInfo.pQueueCreateInfos = &queueInfo;
+    deviceInfo.enabledExtensionCount = 2;
+    deviceInfo.ppEnabledExtensionNames = deviceExtensions;
+
+    VkDevice device;
+    vkCreateDevice(gpu, &deviceInfo, nullptr, &device);
+
+    VkQueue queue;
+    vkGetDeviceQueue(device, queueFamilyIndex, 0, &queue);
+
+    // 4. Swapchain
+    VkSurfaceCapabilitiesKHR caps;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(gpu, surface, &caps);
+
+    VkSurfaceFormatKHR format { VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+    VkSwapchainCreateInfoKHR swapInfo { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
+    swapInfo.surface = surface;
+    swapInfo.minImageCount = 2;
+    swapInfo.imageFormat = format.format;
+    swapInfo.imageColorSpace = format.colorSpace;
+    swapInfo.imageExtent = caps.currentExtent;
+    swapInfo.imageArrayLayers = 1;
+    swapInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapInfo.preTransform = caps.currentTransform;
+    swapInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    swapInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    swapInfo.clipped = VK_TRUE;
+
+    VkSwapchainKHR swapchain;
+    vkCreateSwapchainKHR(device, &swapInfo, nullptr, &swapchain);
+
+    uint32_t imageCount;
+    vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr);
+    std::vector<VkImage> images(imageCount);
+    vkGetSwapchainImagesKHR(device, swapchain, &imageCount, images.data());
+
+    // 5. Image Views
+    std::vector<VkImageView> views(imageCount);
+    for (uint32_t i = 0; i < imageCount; ++i)
+    {
+        VkImageViewCreateInfo viewInfo { VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
+        viewInfo.image = images[i];
+        viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        viewInfo.format = format.format;
+        viewInfo.subresourceRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+        vkCreateImageView(device, &viewInfo, nullptr, &views[i]);
+    }
+
+    // 6. Command Pool + Buffer
+    VkCommandPoolCreateInfo poolInfo { VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO };
+    poolInfo.queueFamilyIndex = queueFamilyIndex;
+
+    VkCommandPool pool;
+    vkCreateCommandPool(device, &poolInfo, nullptr, &pool);
+
+    VkCommandBufferAllocateInfo allocInfo { VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO };
+    allocInfo.commandPool = pool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    VkCommandBuffer cmd;
+    vkAllocateCommandBuffers(device, &allocInfo, &cmd);
+
+    // 7. Sync
+    VkSemaphore imageAvailable, renderFinished;
+    VkFence fence;
+    VkSemaphoreCreateInfo semInfo { VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+    vkCreateSemaphore(device, &semInfo, nullptr, &imageAvailable);
+    vkCreateSemaphore(device, &semInfo, nullptr, &renderFinished);
+
+    VkFenceCreateInfo fenceInfo { VK_STRUCTURE_TYPE_FENCE_CREATE_INFO };
+    vkCreateFence(device, &fenceInfo, nullptr, &fence);
+
+
+
+    std::cout << "Instance: " << instance << std::endl;
+    std::cout << "gpu: " << gpu << std::endl;
+    std::cout << "device: " << device << std::endl;
+    std::cout << "queue: " << queue << std::endl;
+    std::cout << "surface: " << surface << std::endl;
+	std::cout << "swapchain: " << swapchain << std::endl;
+	std::cout << "imageCount: " << imageCount << std::endl;
+	std::cout << "imageAvailable: " << imageAvailable << std::endl;
+	std::cout << "cmd" << cmd << std::endl;
 
 
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+        uint32_t imageIndex;
+        vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, imageAvailable, VK_NULL_HANDLE, &imageIndex);
+
+        VkCommandBufferBeginInfo begin { VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO };
+        vkBeginCommandBuffer(cmd, &begin);
+
+        VkRenderingAttachmentInfo colorAttachment { VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO };
+        colorAttachment.imageView = views[imageIndex];
+        colorAttachment.imageLayout = VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL;
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachment.clearValue.color = { {0.0f, 0.2f, 0.4f, 1.0f} };
+
+        VkRenderingInfo renderInfo { VK_STRUCTURE_TYPE_RENDERING_INFO };
+        renderInfo.renderArea.extent = caps.currentExtent;
+        renderInfo.layerCount = 1;
+        renderInfo.colorAttachmentCount = 1;
+        renderInfo.pColorAttachments = &colorAttachment;
+
+        vkCmdBeginRendering(cmd, &renderInfo);
+        vkCmdEndRendering(cmd);
+        vkEndCommandBuffer(cmd);
+
+        VkPipelineStageFlags waitStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+
+        VkSubmitInfo submit { VK_STRUCTURE_TYPE_SUBMIT_INFO };
+        submit.waitSemaphoreCount = 1;
+        submit.pWaitSemaphores = &imageAvailable;
+        submit.pWaitDstStageMask = &waitStage;
+        submit.commandBufferCount = 1;
+        submit.pCommandBuffers = &cmd;
+        submit.signalSemaphoreCount = 1;
+        submit.pSignalSemaphores = &renderFinished;
+
+        vkResetFences(device, 1, &fence);
+        vkQueueSubmit(queue, 1, &submit, fence);
+        vkWaitForFences(device, 1, &fence, VK_TRUE, UINT64_MAX);
+
+        VkPresentInfoKHR present { VK_STRUCTURE_TYPE_PRESENT_INFO_KHR };
+        present.waitSemaphoreCount = 1;
+        present.pWaitSemaphores = &renderFinished;
+        present.swapchainCount = 1;
+        present.pSwapchains = &swapchain;
+        present.pImageIndices = &imageIndex;
+        vkQueuePresentKHR(queue, &present);
 	}
 
-	Destroy();
+    vkDeviceWaitIdle(device);
+    for (auto view : views) 
+        vkDestroyImageView(device, view, nullptr);
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+    vkDestroyCommandPool(device, pool, nullptr);
+    vkDestroySemaphore(device, imageAvailable, nullptr);
+    vkDestroySemaphore(device, renderFinished, nullptr);
+    vkDestroyFence(device, fence, nullptr);
+    vkDestroyDevice(device, nullptr);
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+    vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
+
 	glfwTerminate();
 }
 
